65305f84243b594a0c1c4aaab07cc891e9e8d7bb
Matthew Belk <mbelk@bizspeed.com>
2019-02-12 07:55:40 -0500

    initial commit

---
+++ XfxSampleApp/XfxSampleApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XfxSampleApp.ViewModels
+{
+	public class MainPageViewModel : ViewModelBase
+	{
+		public MainPageViewModel(INavigationService navigationService)
+			: base(navigationService)
+		{
+			Title = "Main Page";
+		}
+
+        private string _testText;
+        public string TestText
+        {
+            get { return _testText; }
+            set { SetProperty(ref _testText, value); }
+        }
+    }
+}
